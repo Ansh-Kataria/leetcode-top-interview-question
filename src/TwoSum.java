dd7eafb313a483f7b3412c15e3849154394aad03
Ansh Kataria <anshkataria28@gmail.com>
2023-03-14 00:37:52 +0530

    updates

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -3,11 +3,18 @@ import java.util.Map;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+
+        // Create a Hashmap
         Map<Integer, Integer> map = new HashMap<>();
+
+        // loop over all the values and check if map contains the
+        // target - current value
         for (int i=0;i<nums.length;i++){
             if (map.containsKey(target - nums[i])){
                 return new int[]{map.get(target - nums[i]), i};
             }
+
+            // else put the current value into the map as key with index as value
             map.put(nums[i], i);
         }
 
